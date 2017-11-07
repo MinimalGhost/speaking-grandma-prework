2866df41ed50ee401c2d2b57f9d2bb3c55e16cb6
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-07 13:52:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -6,13 +6,13 @@
 def speak_to_grandma(string)
   if string === "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string === string.downcase
+  elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
-  end  
+  end
 end
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
