bb1137bbc730768e13690f496356fe91c9913370
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-07 13:50:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,15 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+def speak_to_grandma(string)
+  if string === "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string === string.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end  
+end
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
